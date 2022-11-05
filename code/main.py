a0b0741d58e7b0adff44eacafd94822cdc4b7d14
Misterpetchz <petch.t1507@gmail.com>
2022-11-05 21:59:16 +0700

    UIupdate

--- code/main.py
+++ code/main.py
@@ -13,7 +13,7 @@ class Game:
 
 		self.level = Level()
 	
-	def run(self):
+	def run(self) :
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
--- code/setting.py
+++ code/setting.py
@@ -4,7 +4,28 @@ HEIGHT = 720
 FPS = 60
 TILESIZE = 64
 
+#ui
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT_SIZE = 18
+UI_FONT = '../Assets/font/Winkle-Regular.ttf'
+
+#general colors
+WATER_COLOR = '#71ddee'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
+
+#ui colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
+
 #weapons 
 weapon_data = {
-    'excalibur' : {'cooldown' : 100, 'damage' : 15 ,'graphic' : '../Assets/weapon/full-up.png'}
-}
+    'excalibur' : {'cooldown' : 100, 'damage' : 15 ,'graphic' : '../Assets/weapon/excalibur/full-up.png'}}
+
+#skill
+magic_data = {}
