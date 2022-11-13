2575ac4cb28516e2cd0e3854de275289a251342f
Misterpetchz <petch.t1507@gmail.com>
2022-11-13 11:51:36 +0700

    particles

--- code/level.py
+++ code/level.py
@@ -97,7 +97,7 @@ class Level:
             self.player.health -= amount
             self.player.vulnerable = False
             self.player.hurt_time = pygame.time.get_ticks()
-            #spawn particles
+            self.animation_player.create_particles(attack_type, self.player.rect.center, [self.visible_sprites])
 
     def run(self):
         # update and draw the game
--- code/particles.py
+++ code/particles.py
@@ -1,5 +1,6 @@
 import pygame
 from support import import_folder
+from random import choice
 
 class AnimationPlayer:
     def __init__(self):
@@ -27,13 +28,18 @@ class AnimationPlayer:
             new_frames.append(flipped_frame)
         return new_frames
 
+    def create_particles(self,animation_type, pos, groups):
+        animation_frames = self.frames[animation_type]
+        ParticleEffect(pos,animation_frames, groups)
+
 class ParticleEffect(pygame.sprite.Sprite):
     def __init__(self, pos, animation_frames, groups):
         super().__init__(groups)
         self.frame_index = 0
         self.animation_speed = 0.15
         self.frames = animation_frames
-        self.image = self.image.get_rect[self.frame_index]
+        self.image = self.frames[self.frame_index]
+        self.rect = self.image.get_rect(center = pos)
 
     def animate(self):
         self.frame_index += self.animation_speed
