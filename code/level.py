3b48f7b3493dd9ffe4d65cf53bbc355ba17ad11c
Misterpetchz <petch.t1507@gmail.com>
2022-11-14 22:06:19 +0700

    -

--- code/level.py
+++ code/level.py
@@ -1,11 +1,10 @@
-from re import X
-import pygame
-import random
+import pygame 
 from setting import *
 from tile import Tile
 from player import Player
 from debug import debug
 from support import *
+from random import choice, randint
 from weapon import Weapon
 from ui import UI
 from enemy import Enemy
@@ -14,162 +13,160 @@ from magic import MagicPlayer
 from upgrade import Upgrade
 
 class Level:
-    def __init__(self) :
-        
-        # get the display surface
-        self.display_surface = pygame.display.get_surface()
-        self.game_paused = False
+	def __init__(self):
 
-        # sprite group setup
-        self.visible_sprites = YSortCameraGroup()
-        self.obstacle_sprites = pygame.sprite.Group()
+		# get the display surface 
+		self.display_surface = pygame.display.get_surface()
+		self.game_paused = False
 
-        # attack sprites
-        self.current_attack = None
-        self.attack_sprites = pygame.sprite.Group()
-        self.attackable_sprites = pygame.sprite.Group()
+		# sprite group setup
+		self.visible_sprites = YSortCameraGroup()
+		self.obstacle_sprites = pygame.sprite.Group()
 
-        # sprite setup
-        self.create_map()
+		# attack sprites
+		self.current_attack = None
+		self.attack_sprites = pygame.sprite.Group()
+		self.attackable_sprites = pygame.sprite.Group()
 
-        #user interface
-        self.ui = UI()
-        self.upgrade = Upgrade(self.player)
+		# sprite setup
+		self.create_map()
 
-        #particles
-        self.animation_player = AnimationPlayer()
-        self.magic_player = MagicPlayer(self.animation_player)
+		# user interface 
+		self.ui = UI()
+		self.upgrade = Upgrade(self.player)
 
-    def create_map(self):
-        layouts = {
-            'boundary' : import_csv_layout('../map/new1_floorblock.csv'),
-            'entities' : import_csv_layout('../map/true1_entities.csv')
-        }
+		# particles
+		self.animation_player = AnimationPlayer()
+		self.magic_player = MagicPlayer(self.animation_player)
 
-        for style,layout in layouts.items():
-            for row_index, row in enumerate(layout):
-                for col_index, col in enumerate(row):
-                    if col != '-1':
-                        x = col_index * TILESIZE
-                        y = row_index * TILESIZE
-                        if style == 'boundary':
-                            Tile((x,y),[self.obstacle_sprites], 'invisible')
-                        
-                        if style == 'entities':
-                            if col == '394':
-                                self.player = Player(
-                                    (x,y),
-                                    [self.visible_sprites],
-                                    self.obstacle_sprites,
-                                    self.create_attack,
-                                    self.destroy_attack,
-                                    self.create_magic)
-                            else:
-                                if col == '390' : monster_name = 'beetle'
-                                elif col == '391' : monster_name = 'maggot'
-                                else : monster_name = 'wyrm'
-                                Enemy(
-                                    monster_name,
-                                    (x,y),
-                                    [self.visible_sprites,self.attackable_sprites],
-                                    self.obstacle_sprites,
-                                    self.damage_player,
-                                    self.trigger_death_particles,
-                                    self.add_exp)
-        
+	def create_map(self):
+		layouts = {
+			'boundary': import_csv_layout('../map/finally_floorblock.csv'),
+			'entities': import_csv_layout('../map/new1_entities.csv')
+		}
+		for style,layout in layouts.items():
+			for row_index,row in enumerate(layout):
+				for col_index, col in enumerate(row):
+					if col != '-1':
+						x = col_index * TILESIZE
+						y = row_index * TILESIZE
+						if style == 'boundary':
+							Tile((x,y),[self.obstacle_sprites],'invisible')
+						
+						if style == 'entities':
+							if col == '394':
+								self.player = Player(
+									(x,y),
+									[self.visible_sprites],
+									self.obstacle_sprites,
+									self.create_attack,
+									self.destroy_attack,
+									self.create_magic)
+							else:
+								if col == '390': monster_name = 'beetle'
+								elif col == '394': monster_name = 'maggot'
+								else: monster_name = 'wyrm'
+								Enemy(
+									monster_name,
+									(x,y),
+									[self.visible_sprites,self.attackable_sprites],
+									self.obstacle_sprites,
+									self.damage_player,
+									self.trigger_death_particles,
+									self.add_exp)
 
-    def create_attack(self):
+	def create_attack(self):
+		
+		self.current_attack = Weapon(self.player,[self.visible_sprites,self.attack_sprites])
 
-        self.current_attack = Weapon(self.player,[self.visible_sprites,self.attack_sprites])
+	def create_magic(self,style,strength,cost):
+		if style == 'heal':
+			self.magic_player.heal(self.player,strength,cost,[self.visible_sprites])
 
-    def destroy_attack(self):
-        if self.current_attack:
-            self.current_attack.kill()
-        self.current_attack = None
+		if style == 'flame':
+			self.magic_player.flame(self.player,cost,[self.visible_sprites,self.attack_sprites])
 
-    def create_magic(self,style,strength,cost):
-        if style == 'crescent':
-            self.magic_player.crescent(self.player,cost,[self.visible_sprites,self.attack_sprites])
-             
-        if style == 'fireball':
-            self.magic_player.fireball(self.player,cost,[self.visible_sprites,self.attack_sprites])
+	def destroy_attack(self):
+		if self.current_attack:
+			self.current_attack.kill()
+		self.current_attack = None
 
-        #if style == 'heal':
-        #   self.magic_player.heal(self.player,strength,cost,[self.visible_sprites])
+	def player_attack_logic(self):
+		if self.attack_sprites:
+			for attack_sprite in self.attack_sprites:
+				collision_sprites = pygame.sprite.spritecollide(attack_sprite,self.attackable_sprites,False)
+				if collision_sprites:
+					for target_sprite in collision_sprites:
+						if target_sprite.sprite_type == 'grass':
+							pos = target_sprite.rect.center
+							offset = pygame.math.Vector2(0,75)
+							for leaf in range(randint(3,6)):
+								self.animation_player.create_grass_particles(pos - offset,[self.visible_sprites])
+							target_sprite.kill()
+						else:
+							target_sprite.get_damage(self.player,attack_sprite.sprite_type)
 
-    def player_attack_logic(self):
-        if self.attack_sprites:
-            for attack_sprites in self.attack_sprites:
-                collision_sprites = pygame.sprite.spritecollide(attack_sprites, self.attackable_sprites,False)
-                if collision_sprites:
-                    for target_sprite in collision_sprites:
-                        if target_sprite.sprite_type == 'enemy':
-                            target_sprite.get_damage(self.player, attack_sprites.sprite_type)
-                            
-    def damage_player(self,amount,attack_type):
-        if self.player.vulnerable:
-            self.player.health -= amount
-            self.player.vulnerable = False
-            self.player.hurt_time = pygame.time.get_ticks()
-            self.animation_player.create_particles(attack_type, self.player.rect.center, [self.visible_sprites])
+	def damage_player(self,amount,attack_type):
+		if self.player.vulnerable:
+			self.player.health -= amount
+			self.player.vulnerable = False
+			self.player.hurt_time = pygame.time.get_ticks()
+			self.animation_player.create_particles(attack_type,self.player.rect.center,[self.visible_sprites])
 
-    def trigger_death_particles(self,pos,particle_type):
+	def trigger_death_particles(self,pos,particle_type):
 
-        self.animation_player.create_particles(particle_type,pos,self.visible_sprites)
+		self.animation_player.create_particles(particle_type,pos,self.visible_sprites)
 
-    def add_exp(self,amount):
+	def add_exp(self,amount):
 
-        self.player.exp += amount
+		self.player.exp += amount
 
-    def toggle_menu(self):
+	def toggle_menu(self):
 
-        self.game_paused = not self.game_paused
+		self.game_paused = not self.game_paused 
 
-    def run(self):
-        self.visible_sprites.custom_draw(self.player)
-        self.ui.display(self.player)
-
-        if self.game_paused:
-            self.upgrade.display()
-        else:
-            self.visible_sprites.update()
-            self.visible_sprites.enemy_update(self.player)
-            self.player_attack_logic()
-        
-      
+	def run(self):
+		self.visible_sprites.custom_draw(self.player)
+		self.ui.display(self.player)
+		
+		if self.game_paused:
+			self.upgrade.display()
+		else:
+			self.visible_sprites.update()
+			self.visible_sprites.enemy_update(self.player)
+			self.player_attack_logic()
+		
 
 class YSortCameraGroup(pygame.sprite.Group):
-    def __init__(self):
+	def __init__(self):
 
-        #general setup
-        super().__init__()
-        self.display_surface = pygame.display.get_surface()
-        self.half_width = self.display_surface.get_size()[0]//2
-        self.half_height = self.display_surface.get_size()[1]//2
-        self.offset = pygame.math.Vector2()
+		# general setup 
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.half_width = self.display_surface.get_size()[0] // 2
+		self.half_height = self.display_surface.get_size()[1] // 2
+		self.offset = pygame.math.Vector2()
 
-        #creating the floor
-        self.floor_surf = pygame.image.load('../Assets/tilemap/castlefarm.png').convert()
-        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+		# creating the floor
+		self.floor_surf = pygame.image.load('../Assets/tilemap/castlefarm.png').convert()
+		self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
 
-   
-    def custom_draw(self,player):
+	def custom_draw(self,player):
 
-        #getting the offset
-        self.offset.x = player.rect.centerx - self.half_width
-        self.offset.y = player.rect.centery - self.half_height
+		# getting the offset 
+		self.offset.x = player.rect.centerx - self.half_width
+		self.offset.y = player.rect.centery - self.half_height
 
-        #drawing the floor
-        floor_offset_pos = self.floor_rect.topleft - self.offset
-        self.display_surface.blit(self.floor_surf,floor_offset_pos)
-        
-        #for sprite in self.sprites():
-        for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
-            offset_pos  = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image,offset_pos)
+		# drawing the floor
+		floor_offset_pos = self.floor_rect.topleft - self.offset
+		self.display_surface.blit(self.floor_surf,floor_offset_pos)
 
+		# for sprite in self.sprites():
+		for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image,offset_pos)
 
-    def enemy_update(self,player):
-        enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite, 'sprite_type') and sprite.sprite_type == 'enemy']
-        for enemy in enemy_sprites:
-            enemy.enemy_update(player)
+	def enemy_update(self,player):
+		enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite,'sprite_type') and sprite.sprite_type == 'enemy']
+		for enemy in enemy_sprites:
+			enemy.enemy_update(player)
--- code/main.py
+++ code/main.py
@@ -1,7 +1,6 @@
 import pygame, sys
 from setting import *
 from level import Level
-import json
 
 class Game:
 	def __init__(self):
@@ -13,34 +12,21 @@ class Game:
 		self.clock = pygame.time.Clock()
 		self.level = Level()
 
-		#sound
-		self.image_screen = pygame.image.load('../Assets/start_menu/screen.png')
-		self.image_score =  pygame.image.load
-		self.font = pygame.font.Font(UI_FONT, 10)
-		self.rect = self.image_screen.get_rect(topleft = (0,0))
-		main_sound = pygame.mixer.Sound('../audio/main.wav')
-		main_sound.set_volume(0.5)
-		main_sound.play(loops = -1)
-		self.text_surf = self.font.render('65010773 Petch Tariyacharoen', False, TEXT_COLOR)
-		self.text_surf = self.text_surf.get_rect(center = ((1280 // 2), 700))
 		
-		
-	
 	def run(self) :
-			while True:
-				
-				for event in pygame.event.get():
-					if event.type == pygame.QUIT:
-						pygame.quit()
-						sys.exit()
-					if event.type == pygame.KEYDOWN:
-						if event.key == pygame.K_m:
-							self.level.toggle_menu()
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_m:
+						self.level.toggle_menu()
 
-					self.screen.fill(WATER_COLOR)
-					self.level.run()
-					pygame.display.update()
-					self.clock.tick(FPS)
+				self.screen.fill(WATER_COLOR)
+				self.level.run()
+				pygame.display.update()
+				self.clock.tick(60)
 
 if __name__ == '__main__':
 	game = Game()
